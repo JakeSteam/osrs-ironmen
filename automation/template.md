da1a3fd2e81de191a4533b074c893d00f4478e70
Jake Lee <github@jakelee.co.uk>
2023-10-05 14:59:56 +0100

    Add untested CI

---
+++ automation/template.md
@@ -0,0 +1,13 @@
+There are too many snowflake accounts to keep up with them all. 
+
+This list updates once a day, feel free to [add any missing playlists](#add-a-playlist). Tap a column's header to sort by that field.
+
+dynamic-playlist-data
+
+## Add a playlist
+
+To add a playlist, edit [`channels.txt`](https://github.com/JakeSteam/osrs-ironmen/blob/main/automation/channels.txt) to include the playlist ID and name:
+* Playlist ID can be retrieved from the playlist's URL (everything after `?list=`).
+* Playlist name is not used, except to make the file more readable.
+
+Alternatively, drop an email to [osrs@jakelee.co.uk](mailto:osrs@jakelee.co.uk) and it'll get added.
---
+++ automation/youtube-update.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+playlists_file="${WORKSPACE}/automation/playlists.txt"
+template_file="${WORKSPACE}/automation/template.md"
+output_file="${WORKSPACE}/README.md"
+
+header_prefix="### "
+placeholder_text="dynamic-playlist-data"
+temp_output_file="output.json"
+output=""
+
+# Convert list of playlists into Markdown tables
+while read -r line; do
+    if [[ ${line} == ${header_prefix}* ]]; then
+        echo "Adding header ${line}"
+        output="${output}\n${line}\n\n"
+        output="${output}| Playlist ↕ | Creator ↕ | # Videos ↕ |\n| --- | --- | --- |\n"
+    else
+        IFS=';' read -r playlist_id playlist_name emoji <<< "${line}" # Split line by semi-colon
+        echo "Adding playlist ${playlist_name} (${playlist_id})"
+        curl "https://www.googleapis.com/youtube/v3/playlists?part=id,statistics,snippet&id=${channel_id}&key=${API_KEY}" \
+            --header 'Accept: application/json' \
+            -fsSL -o ${temp_output_file}
+
+        # Pull playlist data out of response if possible
+        if [[ $(jq -r '.pageInfo.totalResults' output.json) == 1 ]]; then
+            jq_fields=(
+                '.items[0].id'
+                '.items[0].snippet.title'
+                '.items[0].snippet.channelId'
+                '.items[0].snippet.channelTitle'
+                '.items[0].contentDetails.itemCount'
+            )
+            {
+                read -r playlist_id
+                read -r playlist_title
+                read -r channel_id
+                read -r channel_title
+                read -r video_count
+            } < <(IFS=','; jq -r "${jq_fields[*]}" < ${temp_output_file})
+
+            video_count=$(numfmt --to=si "${video_count}" | tr G B)
+            echo "Added ${playlist_title}: ${video_count} videos"
+            output="${output}| ${emoji}[${playlist_title}](https://www.youtube.com/playlist?list=${playlist_id}) | [${channel_title}](https://www.youtube.com/${channel_id}) | ${video_count} |\n"
+        else
+            echo "Failed! Bad response received: $(<${temp_output_file})"
+            exit 1
+        fi
+    fi
+done < ${playlists_file}
+
+# Replace placeholder in template with output, updating the README
+template_contents=$(<${template_file})
+echo -e "${template_contents//${placeholder_text}/${output}}" > ${output_file}
+
+# Debug
+cat "${WORKSPACE}/README.md"
