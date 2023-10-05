535779d8e9c9ead84352254a037f07f78c22bd4b
Jake Lee <github@jakelee.co.uk>
2023-10-05 15:08:18 +0100

    Fix title, perhaps fix API call

--- automation/youtube-update.sh
+++ automation/youtube-update.sh
@@ -18,21 +18,19 @@ while read -r line; do
     else
         IFS=';' read -r playlist_id playlist_name emoji <<< "${line}" # Split line by semi-colon
         echo "Adding playlist ${playlist_name} (${playlist_id})"
-        curl "https://www.googleapis.com/youtube/v3/playlists?part=id,statistics,snippet&id=${channel_id}&key=${API_KEY}" \
+        curl "https://youtube.googleapis.com/youtube/v3/playlists?part=statistics,snippet&id=${playlist_id}&key=${API_KEY}" \
             --header 'Accept: application/json' \
             -fsSL -o ${temp_output_file}
 
         # Pull playlist data out of response if possible
         if [[ $(jq -r '.pageInfo.totalResults' output.json) == 1 ]]; then
             jq_fields=(
-                '.items[0].id'
                 '.items[0].snippet.title'
                 '.items[0].snippet.channelId'
                 '.items[0].snippet.channelTitle'
                 '.items[0].contentDetails.itemCount'
             )
             {
-                read -r playlist_id
                 read -r playlist_title
                 read -r channel_id
                 read -r channel_title
