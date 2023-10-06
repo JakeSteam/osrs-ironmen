850e57cefd2afc030e1c285e8a643c985bf0d1da
Jake Lee <github@jakelee.co.uk>
2023-10-06 14:34:49 +0100

    tidy up js / formatting

--- js/image_hiding.js
+++ js/image_hiding.js
@@ -1,7 +1,3 @@
-window.addEventListener("load", function() {
-  toggleImages();
-}, false);
-
 function toggleImages() {
   document.querySelectorAll('img')
     .forEach(i => i.hidden = !i.hidden);
